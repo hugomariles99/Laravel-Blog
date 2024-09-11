b150607035b70347d34a599310ba1dee508d0fbf
Hugo Mariles <inghugomariles@gmail.com>
2024-09-11 12:51:33 -0600

    First commit of Laravel "Blog" project

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('cursos', function () {
+    return "Bienvenido a la pagina de cursos";
+});
