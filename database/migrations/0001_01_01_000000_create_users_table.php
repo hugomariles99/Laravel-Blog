dc66f411382b2284bff364103849a97805557664
Hugo Mariles <inghugomariles@gmail.com>
2024-09-13 11:52:05 -0600

    Adding Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/*
+    TERMINAL
+    php artisan migrate
+*/
+
 return new class extends Migration
 {
     /**
@@ -12,13 +17,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id(); //Integer Unsigned Increment
+            $table->string('name'); //Varchar 255chars
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //created_at updated_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
